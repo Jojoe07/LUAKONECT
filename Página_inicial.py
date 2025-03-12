4fd380ffb482378451deab3fb451792a5a982dd4
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-03-12 12:21:11 -0300

    Update Página_inicial.py

--- Página_inicial.py
+++ Página_inicial.py
@@ -7,8 +7,5 @@ Dicas de como acessar nas ferramentas:
 - 360 = BP + senha
 - Genesis  = conta microsoft > collaborte / communicate
 
-Para facilitar no atendimento temos a célula baby, clique em saiba mais e será direcionado para o site.
+Para facilitar no atendimento temos a célula baby
 """)
-
-
-st.link_button("Saiba mais", "https://sites.google.com/outsourcing-account.com/celulababy/p%C3%A1gina-inicial")
