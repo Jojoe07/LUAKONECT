d61bf1c03d9836e5f4022819fab72e0c506462bd
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-03-12 12:11:49 -0300

    Update Página_inicial.py

--- Página_inicial.py
+++ Página_inicial.py
@@ -1,1 +1,14 @@
+("""
 
+Dicas de como acessar nas ferramentas: 
+
+- Didatik = cpf + senha = Konecta@2025
+- Intergrall = RK + cpf
+- 360 = BP + senha
+- Genesis  = conta microsoft > collaborte / communicate
+
+Para facilitar no atendimento temos a célula baby, clique em saiba mais e será direcionado para o site.
+""")
+
+
+st.link_button("Saiba mais", "https://sites.google.com/outsourcing-account.com/celulababy/p%C3%A1gina-inicial")
